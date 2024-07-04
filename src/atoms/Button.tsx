0fa97223f97f8ccbf1aeb9701c64ac7041b2f1db
Franco Di Leo <dileo.francoj@gmail.com>
2024-07-04 08:14:02 -0300

    chore: create very first atom

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import { type Props } from './Button.types'
 
 export const Button: React.FC<Props> = ({ variant = 'primary', onClick, label, textColor }) => {
+  console.log('variant, textColor', variant, textColor)
   return (
     <button onClick={onClick}>
         <span>{label}</span>
